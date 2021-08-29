81004eeb6d8444ca4862eb381ba0c81a0a61fafa
김다은 <softeun92@naver.com>
2021-08-29 16:19:26 +0900

    새로 입력한 닉네임을 들고 -> 메인으로 복귀   - 메인에서 반영 X, 새 닉네임 반영 안됨.

--- app/src/main/java/com/nepplus/intent_20210829/EditNicknameActivity.kt
+++ app/src/main/java/com/nepplus/intent_20210829/EditNicknameActivity.kt
@@ -2,10 +2,39 @@ package com.nepplus.intent_20210829
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        okBtn.setOnClickListener {
+            //확인 버튼이 눌리면?
+            //  입력한 닉네임이 뭔지 변수에 저장
+
+            val inputNickname = nicknameEdt.text.toString()
+
+            //// 그변수를 들고 이전화면으로 복귀 (figish())
+            //들고 : 데이터 첨부 -> Itent putExtra  활용
+
+            val reslutIntent = Intent()
+
+            //닉네임 첨부
+
+            reslutIntent.putExtra("newNickname", inputNickname)
+
+            //완료 ( Ok) 누른게 맞다 + Intent 세팅. => 이화면의 결과 지정.
+            setResult(RESULT_OK, resultIntent)
+
+            finish()
+
+
+
+
+
+
+        }
+
     }
 }
