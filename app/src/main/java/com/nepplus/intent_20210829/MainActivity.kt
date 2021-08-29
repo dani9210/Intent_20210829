1c17b2af81a6cacf21824db7b662acafd0436dd8
김다은 <softeun92@naver.com>
2021-08-29 15:30:47 +0900

    데이터를 첨부 기능 예시   - myIntent.putExtra

--- app/src/main/java/com/nepplus/intent_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_20210829/MainActivity.kt
@@ -23,6 +23,9 @@ class MainActivity : AppCompatActivity() {
             // 메세지 조회화면으로 (입력한 내용을 들고) 이동.
 
             val myIntent = Intent(this, ViewMessageActivity::class.java)
+
+            myIntent.putExtra("inputMessage", inputContent)
+
             startActivity(myIntent)
 
         }
